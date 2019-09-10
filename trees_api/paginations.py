17c872633566b8ffb0ee6854e3b0d1d3808a10e5
scrambldchannel <41974784+scrambldchannel@users.noreply.github.com>
2019-09-10 11:41:54 +0200

    Increase pagination size

--- trees_api/paginations.py
+++ trees_api/paginations.py
@@ -4,10 +4,10 @@ from rest_framework_gis import pagination as gis_pagination
 
 
 class PageNumberPagination(pagination.PageNumberPagination):
-    page_size = 10
+    page_size = 100
     page_size_query_param = 'page_size'
 
 
 class GeoJsonPagination(gis_pagination.GeoJsonPagination):
-    page_size = 10
+    page_size = 100
     page_size_query_param = 'page_size'
